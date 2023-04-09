85b3d6e11766c1800232b8bd6592845068582866
Ritwik Vaidya <vaidyaritwik@gmail.com>
2023-04-09 17:24:47 -0400

    fix podcast path

--- backend/src/main/java/edu/ncsu/csc540/s23/backend/controller/PodcastController.java
+++ backend/src/main/java/edu/ncsu/csc540/s23/backend/controller/PodcastController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("podcast")
+@RequestMapping("/podcast")
 public class PodcastController {
 
     private PodcastService podcastService;
