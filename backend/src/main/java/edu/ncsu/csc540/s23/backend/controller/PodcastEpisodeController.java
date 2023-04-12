d87380f85a7e19e4945244e18e75f6c303c78c33
Ritwik Vaidya <vaidyaritwik@gmail.com>
2023-04-11 20:30:38 -0400

    final modification!

--- backend/src/main/java/edu/ncsu/csc540/s23/backend/controller/PodcastEpisodeController.java
+++ backend/src/main/java/edu/ncsu/csc540/s23/backend/controller/PodcastEpisodeController.java
@@ -16,8 +16,8 @@ public class PodcastEpisodeController {
     @GetMapping("/all")
     public List<PodcastEpisode> getAllPodcastEpisodes() { return podcastEpisodeService.getAllPodcastEpisodes(); }
 
-    @GetMapping
-    public PodcastEpisode getPodcastEpisode(@RequestParam(name="pepi_id") Long podcastEpisodeId, @RequestParam(name="podcast_id") Long podcastId) { return podcastEpisodeService.getPodcastEpisode(podcastEpisodeId,podcastId); }
+    @GetMapping("/{podcastEpisodeId}/{podcastId}")
+    public PodcastEpisode getPodcastEpisode(@PathVariable Long podcastEpisodeId, @PathVariable Long podcastId) { return podcastEpisodeService.getPodcastEpisode(podcastEpisodeId,podcastId); }
     @PostMapping("/add")
     public Long addPodcastEpisode(@RequestBody PodcastEpisode podcastEpisode) { return podcastEpisodeService.createNewPodcastEpisode(podcastEpisode); }
 
